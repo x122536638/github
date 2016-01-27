72c8cc20b6665cb6b0e96857197153735abcee11
xhq <122536638@qq.com>
2016-01-27 17:55:55 +0800

    哈哈

---
+++ 测试/测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试
+//
+//  Created by 徐汉卿 on 16/1/27.
+//  Copyright © 2016年 jxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试/测试/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  测试
+//
+//  Created by 徐汉卿 on 16/1/27.
+//  Copyright © 2016年 jxk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
