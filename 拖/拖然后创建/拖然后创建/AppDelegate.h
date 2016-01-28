53ed0a099b6f140e5000a85c28160ae462b86429
xhq <122536638@qq.com>
2016-01-28 15:01:59 +0800

    wocao

---
+++ 拖/拖然后创建/拖然后创建/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  拖然后创建
+//
+//  Created by 徐汉卿 on 16/1/28.
+//  Copyright © 2016年 jxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 拖/拖然后创建/拖然后创建/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  拖然后创建
+//
+//  Created by 徐汉卿 on 16/1/28.
+//  Copyright © 2016年 jxk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
