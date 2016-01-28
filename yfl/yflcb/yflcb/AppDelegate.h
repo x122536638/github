a9fc53b36de0898d898ebfbb9a5237cf36ba4de5
xhq <122536638@qq.com>
2016-01-28 11:01:56 +0800

    changes log

---
+++ yfl/yflcb/yflcb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yflcb
+//
+//  Created by 徐汉卿 on 16/1/28.
+//  Copyright © 2016年 jxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
